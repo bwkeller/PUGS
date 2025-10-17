847ef8a6a8ea8051aa49920e7788882a51d9a74b
Ben Keller <malzraa@gmail.com>
2025-10-16 22:53:24 -0500

    Forgot to do the formatter pass.

--- pugs_volume_ic/fix_header.py
+++ pugs_volume_ic/fix_header.py
@@ -5,9 +5,9 @@ from sys import argv
 if __name__ == "__main__":
     fname = argv[1]
     n = 2048**3
-    pad = ((n>>32)&0xFF) + ((n>>16)&0xFF0000) 
+    pad = ((n >> 32) & 0xFF) + ((n >> 16) & 0xFF0000)
     n &= 0xFFFFFFFF
-    with open(fname, 'r+b') as f:
+    with open(fname, "r+b") as f:
         t, n_, ndim, ng_, nd_, ns_, pad_ = struct.unpack("<dIIIIII", f.read(32))
         f.seek(0)
         f.write(struct.pack("<dIIIIII", t, n, ndim, 0, n, 0, pad))
