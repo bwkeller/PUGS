c469c212d0e9b2748d7927123e58c223a25049a8
Ben Keller <malzraa@gmail.com>
2025-10-21 12:37:17 -0500

    Added some previously built scripts that I can hack into the new TANGOS build module.

---
+++ pugs_tangos_db/build_tangos.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+tangos add --min-particles 500 $1
+tangos link --sims $1
+tangos import-properties Mhalo Rhalo lambda r2 cNFW --for $1
+tangos write finder_mass shrink_center --for $1
+tangos write --latest --with-prerequisites z_half --for $1
