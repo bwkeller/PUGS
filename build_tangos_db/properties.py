a740f9bf11e5f305c19a4b6bedc5e84770d6bbf4
Ben Keller <malzraa@gmail.com>
2025-12-12 16:02:33 -0600

    Cleaned up the unlinted code from the last merge.

--- build_tangos_db/properties.py
+++ build_tangos_db/properties.py
@@ -1,34 +1,38 @@
-from tangos.properties.pynbody import PynbodyPropertyCalculation
-from tangos.properties.pynbody.profile import HaloDensityProfile
-from tangos.properties.pynbody.BH import BHAccHistogram
-from tangos.properties.pynbody.centring import centred_calculation
-from tangos.properties import PropertyCalculation, LivePropertyCalculation, TimeChunkedProperty
-from tangos.live_calculation import NoResultsError
-import pynbody
-import numpy as np
 import zlib
-import scipy
+
+import numpy as np
+import pynbody
+from tangos.properties import LivePropertyCalculation
+from tangos.properties.pynbody import PynbodyPropertyCalculation
+
 
 class StoreIords(PynbodyPropertyCalculation):
     names = "zlib_ids"
 
     def calculate(self, particle_data, existing_properties):
-        iords =  np.frombuffer(zlib.compress(particle_data.get_index_list(particle_data.ancestor).tobytes()), dtype=np.int8)
+        iords = np.frombuffer(
+            zlib.compress(particle_data.get_index_list(particle_data.ancestor).tobytes()),
+            dtype=np.int8,
+        )
         return iords
 
     def region_specification(self, existing_properties):
-        return pynbody.filt.Sphere(existing_properties['max_radius']*2,
-                                   existing_properties['shrink_center'])
+        return pynbody.filt.Sphere(
+            existing_properties["max_radius"] * 2, existing_properties["shrink_center"]
+        )
 
     def requires_property(self):
-            return ["shrink_center", "max_radius"]
+        return ["shrink_center", "max_radius"]
+
 
 class GetIords(LivePropertyCalculation):
     names = "ids"
 
     def calculate(self, particle_data, existing_properties):
-        iords = np.frombuffer(zlib.decompress(existing_properties["zlib_ids"].tobytes()), dtype=np.int64)
+        iords = np.frombuffer(
+            zlib.decompress(existing_properties["zlib_ids"].tobytes()), dtype=np.int64
+        )
         return iords
 
     def requires_property(self):
-            return ["zlib_ids"]
+        return ["zlib_ids"]
