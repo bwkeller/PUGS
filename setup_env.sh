40730ca312f98a139e40d4e7df1df3e6c513f4f5
Ben Keller <malzraa@gmail.com>
2026-01-14 15:27:04 -0600

    Refactored the dependencies: everything is now just a plain-jane standard pyproject.toml, no more weird scripts.

--- setup_env.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Initialize our virtual environment
-python3 -m venv .venv
-source .venv/bin/activate
-
-# Install pinned packages
-pip install -r pip-requirements.txt
-pip install -r build-requirements.txt
