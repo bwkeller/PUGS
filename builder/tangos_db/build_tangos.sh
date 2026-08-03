e98dfbaa1acc144de7ead14eeff73db3e63fddc2
Ben Keller <malzraa@gmail.com>
2026-08-03 07:45:48 -0500

    Added the ability to choose number of MPI processes and run the tangos write in parallel.
    Import all AHF properties from the halo finder catalogs.
    
    Use "--quicker" for tangos add to prevent expensive resolution
    calculation.

--- builder/tangos_db/build_tangos.sh
+++ builder/tangos_db/build_tangos.sh
@@ -4,15 +4,20 @@ set -e
 # shellcheck source=/dev/null
 source "$1"
 
-tangos add --min-particles 500 "$SIM"
+if [ -d "$PUGS_NPROCS" ]
+then
+    RUNNER="mpirun -np $PUGS_NPROCS"
+fi
 
-tangos import-properties hostHalo numSubStruct Vmax Rmax Mhalo cNFW lambda lambdaE mbp_offset com_offset sigV c b Ekin Epot
+tangos add --quicker --min-particles 500 "$SIM"
+
+tangos import-properties hostHalo numSubStruct Mhalo Rhalo Rmax r2 mbp_offset com_offset Vmax v_esc sigV lambda lambdaE Lx Ly Lz b c Eax Eay Eaz Ebx Eby Ebz Ecx Ecy Ecz ovdens Ekin Epot SurfP Phi0 cNFW
 
 tangos import-ahf-trees
 
 tangos remove-duplicates
 
-tangos write finder_mass shrink_center max_radius R500 M200 M500 --for "$SIM" --with-prerequisites
+"$RUNNER" tangos write finder_mass shrink_center max_radius M200 --for "$SIM" --with-prerequisites --backend mpi4py --load-mode server-shared-mem
 
 # write needed properties for only the last (z=0) snapshot
 tangos write zlib_ids Rvir_indices N_mm z_lmm z25_mass z50_mass z75_mass --with-prerequisites --for "$SIM" --latest
