a5f52bdac0eb7eee381ad0b161e74630ad873f06
Ben Keller <malzraa@gmail.com>
2026-08-03 08:47:37 -0500

    Script bugfixes for parallel tangos write.

--- builder/tangos_db/build_tangos.sh
+++ builder/tangos_db/build_tangos.sh
@@ -4,9 +4,12 @@ set -e
 # shellcheck source=/dev/null
 source "$1"
 
-if [ -d "$PUGS_NPROCS" ]
+RUNNER=(tangos)
+MPI_FLAGS=()
+if [ -n "$PUGS_NPROCS" ]
 then
-    RUNNER="mpirun -np $PUGS_NPROCS"
+    RUNNER=(mpirun -np "$PUGS_NPROCS" tangos)
+    MPI_FLAGS=(--backend mpi4py --load-mode server-shared-mem)
 fi
 
 tangos add --quicker --min-particles 500 "$SIM"
@@ -17,7 +20,7 @@ tangos import-ahf-trees
 
 tangos remove-duplicates
 
-"$RUNNER" tangos write finder_mass shrink_center max_radius R200 R500 M500 M200 --for "$SIM" --with-prerequisites --backend mpi4py --load-mode server-shared-mem
+"${RUNNER[@]}" write finder_mass shrink_center max_radius R200 R500 M500 M200 --for "$SIM" --with-prerequisites "${MPI_FLAGS[@]}"
 
 # write needed properties for only the last (z=0) snapshot
-tangos write zlib_ids Rvir_indices N_mm z_lmm z25_mass z50_mass z75_mass --with-prerequisites --for "$SIM" --latest
+"${RUNNER[@]}" write zlib_ids Rvir_indices N_mm z_lmm z25_mass z50_mass z75_mass --with-prerequisites --for "$SIM" --latest "${MPI_FLAGS[@]}"
